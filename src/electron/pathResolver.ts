6ec18416429571b981efefbc25f7c6d088ff066e
Sarthak <messagetosarthak@gmail.com>
2026-06-07 12:32:12 +0530

    Preload script not loading fixed, dev script wasn't transpiling electron

--- src/electron/pathResolver.ts
+++ src/electron/pathResolver.ts
@@ -3,6 +3,7 @@ import { app } from 'electron';
 import { isDev } from './utils.js';
 
 export function getPreloadPath() {
+  console.log("path resolver is runnning");
   return path.join(app.getAppPath(), 
   isDev() ? '.' : '..', 
   '/dist-electron/preload.cjs'
